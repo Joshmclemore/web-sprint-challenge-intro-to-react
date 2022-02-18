6f31c31aea417bfe58a9c4a706edd83012a26691
Joshmclemore <josh_mclemore@yahoo.com>
2022-02-18 10:40:02 -0600

    Finished adding styles to my character component. Final push unless I attempt stretch

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
       <Character everyCharacter={characters}/>
     </div>
   );
--- src/components/Character.js
+++ src/components/Character.js
@@ -2,18 +2,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledWrapper = styled.div`
+    margin: 15px;
+    border: 5px solid #89FC00;
+    border-radius: 10%;
+    padding: 10px;
 
+    h2 {
+        border-bottom: 2px solid red;
+    }
+
+    p {
+        text-align: left;
+    }
+`
 
 const Character = (props) => {
     return (
         <>
             {props.everyCharacter.map(character => {
                 return (
-                <div key={character.url} className="character-wrapper">
+                <StyledWrapper key={character.url} className="character-wrapper">
                     <h2>{character.name}</h2>
-                    <h3>Home World: {character.homeworld}</h3>
+                    <h3>Link to Home World: {character.homeworld}</h3>
                     <p>Films: {character.films.join(', ')}</p>
-                </div>
+                </StyledWrapper>
                 )
             })}
          </> 
